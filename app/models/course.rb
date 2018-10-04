cc733f7d85d2597ae452482d66dc64dbde49ec1f
DKL <dyip@msudenver.edu>
2018-10-04 17:24:27 -0600

    added validations for course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,6 @@
 class Course < ActiveRecord::Base
+  validates :name, :course_number, uniqueness: true
+  validates :name, :department, :course_number, :credit_hours, presence: true
+
   has_many :sections
 end
