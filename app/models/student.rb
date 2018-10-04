b1ba9f3e1b7003991ae8af1e0a532baa9c7618d0
DKL <dyip@msudenver.edu>
2018-10-04 17:26:41 -0600

    added validations for students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,4 +1,9 @@
 class Student < ActiveRecord::Base
+  validates :id_number, uniqueness:true
+  validates :name, :id_number, presence:true
+  validates :id_number, length: {is: 9}
+  validates :name, length: {minimum: 2}  
+
   has_many :enrollments
   has_many :sections, through: :enrollments
 end
